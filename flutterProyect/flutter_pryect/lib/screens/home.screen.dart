be2c8c414d4d01c108d4b0b45bd777ec34d2d762
YakeCode <y.gaviriaescobar@gmail.com>
2024-12-29 22:09:42 -0500

    entendiendo como funciona la programacion con flutter

--- flutterProyect/flutter_pryect/lib/screens/home.screen.dart
+++ flutterProyect/flutter_pryect/lib/screens/home.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const String author = 'Yake';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -8,35 +10,66 @@ class HomeScreen extends StatelessWidget {
     return DefaultTabController(
         length: 4,
         child: Scaffold(
-            body: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 125,
-                child: Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        height: 125,
-                        width: 100,
-                        child: ClipRRect(
-                          //Permite bordes redondeados
-                          borderRadius: BorderRadius.circular(10),
-                          child: Container(),
+            body: Column(
+          children: <Widget>[
+            _RecipeCard(context),
+            _RecipeCard(context),
+          ],
+        )));
+  }
+
+  Widget _RecipeCard(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0), //padding de 8.0 en todos los lados
+        child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 125,
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 125,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ClipRRect(
+                      //Permite bordes redondeados
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 26,
+                  ),
+                  Column(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center, //centra los elementos
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, //alinea los elementos internos a la izquierda
+                      children: <Widget>[
+                        Text(
+                          'Lasagna',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      SizedBox(
-                        width: 26,
-                      ),
-                      Column(children: <Widget>[
-                        Text('lasagna'),
-                        Text('autor : YaKe'),
+                        SizedBox(
+                          height: 4,
+                        ),
                         Container(
                           height: 1,
                           width: 75,
                           color: Colors.orange,
-                        )
+                        ),
+                        Text(
+                          'autor: ${author}',
+                        ),
                       ])
-                    ],
-                  ),
-                ))));
+                ],
+              ),
+            )));
   }
 }
